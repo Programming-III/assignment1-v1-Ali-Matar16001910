9c6528a5bbfe7acdad1d5baf860facb8fff6e966
Ali-Matar16001910 <ali.khaled.matar@gmail.com>
2025-11-12 15:06:01 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,21 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
 
-#include "Animal.h"
-
-//define mammal class here
+#include <Animal.h>
 
 
+class Mammal : public Animal
+{
+    public:
+    Mammal();
+    Mammal(string n, int a, bool h, string f);
+    virtual ~Mammal();
+    string getFurColor();
+    void setFurColor(string f);
+    protected:
 
+    private:
+    string furColor;
+};
 
-
-
-#endif
+#endif // MAMMAL_H
